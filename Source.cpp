8a32185c70d28702b6625d7c9c488dddc8e8a14c
Savely <34402190+Savik89@users.noreply.github.com>
2023-11-17 00:55:01 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -21,19 +21,12 @@ void cloneArray(int arr[], int arr2[])
 }
 void bubbleSort(int arr[], int n)
 {
-	int count = 0;
-	while (true)
-	{
-		count = 0;
-		for (int i = 0; i < n - 1; i++)
-			if (arr[i] > arr[i + 1])
-			{
-				swap(arr[i], arr[i+1]);
-				count++;
-			}
-		if (count == 0)
-			break;
-	}
+	for (int i = 0; i < n-1; ++i)
+		for (int j = 0; j < n - i - 1; ++j)
+		{
+			if (arr[j] > arr[j + 1])
+				swap(arr[j], arr[j + 1]);
+		}
 }
 void shakerSort(int arr[], int n)
 {
